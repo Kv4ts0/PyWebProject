f30b80d6c0af0aa3b6336808da9c1030ef818659
atlantas1 <46217625+atlantas1@users.noreply.github.com>
2019-04-27 17:16:42 +0400

    Update views.py

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 	def index(request):
+		url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
+		city = 'Las Vegas'
+
 		return render(request, 'weather/weather.html')
