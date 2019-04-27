ecca5fd04ba4239b9b2eaff6e845bd5b8af7cab2
Nikushaqvavacabaia <46217588+Nikushaqvavacabaia@users.noreply.github.com>
2019-04-27 17:23:18 +0400

    Update views.py
    e have description about our city and its weather,but we can't see this at main page.Because we have an information in our Power shell/
    ubuntu terrminal ...

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 	def index(request):
 		url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
 		city = 'Las Vegas'
-
+		
+		r=requests.get(url.format(city))
+    		print(r.text)
 		return render(request, 'weather/weather.html')
