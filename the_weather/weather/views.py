d963db73c34280d0f33677dbcbd1106a7062b7b1
Razma (კოკოი) <48160687+razmanika@users.noreply.github.com>
2019-04-27 18:43:03 +0400

    Update views.py
    We can add cities from main page

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,14 +1,21 @@
 import requests
 from django.shortcuts import render
 from .models import City
+from .forms import CityForm
 
 def index(request):
 	url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
-	city = 'Las Vegas'
 	
-	cities = City.objects.all()
+	if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+	form = CityForm()
+
+    	cities = City.objects.all()
 	
 	weather_data = []
+	
 	for city in cities :
 		
 		r=requests.get(url.format(city)).json()
@@ -20,6 +27,7 @@ def index(request):
            		'icon' : r['weather'][0]['icon']
 		}
 		
+		weather_data.append(city_weather)
 		
-    	context = {'city_weather' : city_weather}
+    	context = {'weather_data' : weather_data, 'forms' : forms }
 	return render(request, 'weather/weather.html' context)
