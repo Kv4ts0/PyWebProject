36a36d287efb464dcbc4fc8978bd9d3f0f77a000
lukamuchiashvili <50011959+lukamuchiashvili@users.noreply.github.com>
2019-05-03 14:48:21 +0400

    Add files via upload

---
+++ the_weather/weather/views.py
@@ -0,0 +1,33 @@
+import requests
+from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        r=requests.get(url.format(city)).json()
+
+        city_weather = {
+           'city' : city.name,
+           'temperature' : r['main']['temp'],
+           'description' : r['weather'][0]['description'],
+           'icon' : r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+
+    context = {'weather_data' : weather_data, 'form' : form}
+    return render(request, 'weather/weather.html',context)
